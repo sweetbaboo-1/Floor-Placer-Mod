ef02d9c7ed830e824867a7b80b6f080d0282dcfa
sweetbaboo-1 <72481120+sweetbaboo-1@users.noreply.github.com>
2024-02-09 20:41:41 -0700

    Initial commit

---
+++ src/main/java/net/sweetbaboo/floorplacermod/FloorPlacerModClient.java
@@ -0,0 +1,10 @@
+package net.sweetbaboo.floorplacermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FloorPlacerModClient implements ClientModInitializer {
+  @Override
+  public void onInitializeClient() {
+
+  }
+}
---
+++ src/main/java/net/sweetbaboo/floorplacermod/FloorPlacerModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sweetbaboo.floorplacermod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FloorPlacerModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
